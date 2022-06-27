90cbb33fa3ffe514faf49f2e8928988049c0c434
guillermomorini <guillermo.morini@gmail.com>
2022-06-27 19:32:34 -0400

    Adding ami to ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "First-EC2" {
-    subnet_id = aws_subnet.first_subnet
+    subnet_id     = aws_subnet.first_subnet
     instance_type = "t2.micro"
+    ami           = "ami-2757f631"
 }
