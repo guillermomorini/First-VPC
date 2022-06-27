8da898cd1af2e9e74599e17abc132b2d90394a1b
guillermomorini <guillermo.morini@gmail.com>
2022-06-27 19:46:26 -0400

    adding subnet id

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "First-EC2" {
-    subnet_id     = aws_subnet.first_subnet
+    subnet_id     = aws_subnet.first_subnet.id
     instance_type = "t2.micro"
     ami           = "ami-2757f631"
 }
