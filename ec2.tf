1a474ccef8c348e91d329ce31ce044938414a369
guillermomorini <guillermo.morini@gmail.com>
2022-06-30 14:42:39 -0400

    delete ec2s

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "First-EC2" {
-    subnet_id     = aws_subnet.first_subnet.id
-    instance_type = "t2.micro"
-    ami           = "ami-2757f631"
-}
+#resource "aws_instance" "First-EC2" {
+#    subnet_id     = aws_subnet.first_subnet.id
+#    instance_type = "t2.micro"
+#    ami           = "ami-2757f631"
+#}
 
-resource "aws_instance" "Second-EC2" {
-    subnet_id = aws_subnet.second_subnet.id
-    instance_type = "t2.micro"
-    ami           = "ami-2757f631"
-}
+#resource "aws_instance" "Second-EC2" {
+#    subnet_id = aws_subnet.second_subnet.id
+#    instance_type = "t2.micro"
+#    ami           = "ami-2757f631"
+#}
