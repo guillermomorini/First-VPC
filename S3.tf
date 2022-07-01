c75271754e790805d039832c83ae6c88608e2cd0
guillermomorini <guillermo.morini@gmail.com>
2022-06-30 20:32:06 -0400

    updating s3

--- S3.tf
+++ S3.tf
@@ -1,3 +1,17 @@
 resource "aws_s3_bucket" "first_bucket" {
     bucket = "gmorini-first-bucket"
+    acl = "public-read"
+    policy = templatefile("templates/s3-policy.json", {bucket = "www.gmorini.org"})
+    
+    cors_rule {
+        allowed_headers = ["Authorization", "Content-length"]
+        allowed_methods = ["GET", "POST"]
+        allowed_origins = ["https://www.gmorini.org"]
+        max_age_seconds = 3000
+    }
+    
+    website {
+      index_document = "index.html"
+      error_document = "404.html"
+    }
 }
