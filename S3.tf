15b647d0389d0d0dba700ffe67bdd46372cb0808
guillermomorini <guillermo.morini@gmail.com>
2022-06-30 21:08:14 -0400

    updating to new terraform version of s3

--- S3.tf
+++ S3.tf
@@ -1,17 +1,30 @@
 resource "aws_s3_bucket" "first_bucket" {
     bucket = "gmorini-first-bucket"
     acl = "public-read"
-    policy = templatefile("templates/s3-policy.json", {bucket = "www.gmorini.org"})
-    
+}
+
+resource "aws_s3_bucket_cors_configuration" "cors_config" {
+    bucket = aws_s3_bucket.first_bucket
     cors_rule {
         allowed_headers = ["Authorization", "Content-length"]
         allowed_methods = ["GET", "POST"]
         allowed_origins = ["https://www.gmorini.org"]
         max_age_seconds = 3000
     }
-    
-    website {
-      index_document = "index.html"
-      error_document = "404.html"
-    }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.first_bucket
+  
+  index_document {
+    suffix = "index.html"
+  }
+  
+  error_document {
+    key = "404.html"
+  }
+}
+resource "aws_s3_bucket_policy" "bucket_policy" {
+    bucket = aws_s3_bucket.first_bucket.id
+    policy = templatefile("templates/s3-policy.json", { bucket = "www.gmorini.org"})
 }
