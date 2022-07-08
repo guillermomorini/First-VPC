7930347dee136140a57f18cc6c775e67cd1344ea
guillermomorini <guillermo.morini@gmail.com>
2022-07-08 10:03:45 -0400

    updating app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and ve to reload.
+          Edit <code>src/App.js</code> and 0ve to reload.
         </p>
         <a
           className="App-link"
