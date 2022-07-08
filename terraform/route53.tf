13cfc7f013c731d716b7779885788966d2be7d7e
guillermomorini <guillermo.morini@gmail.com>
2022-07-08 11:01:33 -0400

    adding route53 code to terraform.

---
+++ terraform/route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "gmorini" {
+  name = "gmorini.org"
+}
+
+resource "aws_route53_record" "s3site" {
+  zone_id = aws_route53_zone.gmorini.zone_id
+  name = "gmorini.org"
+  type = "A"
+  
+  alias {
+    name = aws_s3_bucket.first_bucket.s3_bucket_website_domain
+    zone_id = aws_route53_zone.gmorini.zone_id
+    evaluate_target_health = false
+  }
+}
