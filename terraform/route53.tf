7e824c831e2498f6cf8ec30be64bcebb25d9d020
guillermomorini <guillermo.morini@gmail.com>
2022-07-08 11:46:33 -0400

    updating alias name.

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "s3site" {
   type = "A"
   
   alias {
-    name = "s3-website-us-east-1.amazonaws.com"
+    name = "s3-website.us-east-1.amazonaws.com"
     zone_id = aws_route53_zone.gmorini.zone_id
     evaluate_target_health = false
   }
