3b69b89296367a6844a115522de0c0d91344523b
guillermomorini <guillermo.morini@gmail.com>
2022-07-09 15:34:54 -0400

    cert validation

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -2,3 +2,28 @@ resource "aws_acm_certificate" "gmorini_cert" {
     domain_name = "gmorini.org"
     validation_method = "DNS"
 }
+
+resource "aws_route53_record" "cert_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.gmorini_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.this.zone_id
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  timeouts {
+    create = "5m"
+  }
+  certificate_arn         = aws_acm_certificate.gmorini_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
+}
