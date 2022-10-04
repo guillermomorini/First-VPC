739c873cf7478e6c42d3ce103ff9a72ee60224a6
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 09:32:59 -0400

    added resource id to method

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_resource" "dog" {
 
 resource "aws_api_gateway_method" "dog_get_method" {
   rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_resource.dog
+  resource_id = aws_api_gateway_resource.dog.id
   http_method = "GET"
   authorization = "NONE"
 }
