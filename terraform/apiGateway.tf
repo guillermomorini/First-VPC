f3aedd66843c24d771f2ab6a7ccdbcd42a49f225
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 09:39:19 -0400

    adding resource id to post method

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -17,7 +17,7 @@ resource "aws_api_gateway_method" "dog_get_method" {
 
 resource "aws_api_gateway_method" "dog_post"{
     rest_api_id = aws_api_gateway_rest_api.example.id
-    resource_id = aws_api_gateway_resource.dog
+    resource_id = aws_api_gateway_resource.dog.id
     http_method = "POST"
     authorization = "NONE"
 }
