15d52477c2a923cc30e5fcf503210de017be32e2
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 09:31:26 -0400

    adding parent id to gateway resource

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -4,6 +4,7 @@ resource "aws_api_gateway_rest_api" "example" {
 
 resource "aws_api_gateway_resource" "dog" {
     rest_api_id = aws_api_gateway_rest_api.example.id
+    parent_id = aws_api_gateway_rest_api.example.root_resource_id
     path_part = "dog" 
 }
 
