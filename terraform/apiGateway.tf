9ce580f28a9d2833f746bf289fe969567921bdd5
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 14:27:24 -0400

    adding response for dog get method

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -21,3 +21,10 @@ resource "aws_api_gateway_method" "dog_post"{
     http_method = "POST"
     authorization = "NONE"
 }
+
+resource "aws_api_gateway_method_response" "check_in_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.chedogck_in_resource.id
+  http_method = aws_api_gateway_method.dog_get_method.http_method
+  status_code = 200
+}
