b009b0fbf40962b87ec2c51cdd5af05a195584ff
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 09:29:15 -0400

    adding dog resource and get method

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -1,3 +1,15 @@
 resource "aws_api_gateway_rest_api" "example" {
   name = "example"
 }
+
+resource "aws_api_gateway_resource" "dog" {
+    rest_api_id = aws_api_gateway_rest_api.example.id
+    path_part = "dog" 
+}
+
+resource "aws_api_gateway_method" "dog_get_method" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.dog
+  http_method = "GET"
+  authorization = "NONE"
+}
