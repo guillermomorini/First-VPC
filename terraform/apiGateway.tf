f7d9f64c6e40dfdb0c3e44f24eb11bb73b26c9f1
guillermomorini <guillermo.morini@gmail.com>
2022-10-04 09:38:22 -0400

    adding dog post method

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -14,3 +14,10 @@ resource "aws_api_gateway_method" "dog_get_method" {
   http_method = "GET"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_method" "dog_post"{
+    rest_api_id = aws_api_gateway_rest_api.example.id
+    resource_id = aws_api_gateway_resource.dog
+    http_method = "POST"
+    authorization = "NONE"
+}
