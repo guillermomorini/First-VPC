21d48004fb0c8bdbe0420196a8616fbe79ef9081
guillermomorini <guillermo.morini@gmail.com>
2022-11-26 11:35:39 -0500

    update labda name

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "lambda_code_zip" {
 
 resource "aws_lambda_function" "terraform_lambda_func" {
     filename                       = "${path.module}/lambda/lambdacode.zip"
-    function_name                  = "Spacelift_Test_Lambda_Function"
+    function_name                  = "Test_Lambda_Function"
     role                           = aws_iam_role.lambda_role.arn
     handler                        = "index.lambda_handler"
     runtime                        = "python3.8"
