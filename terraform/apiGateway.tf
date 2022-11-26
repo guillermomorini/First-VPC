25e7be20a057005dd240e61e3568aaa5ca0c49ac
guillermomorini <guillermo.morini@gmail.com>
2022-11-26 12:16:54 -0500

    adding delete and put

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -54,3 +54,17 @@ resource "aws_api_gateway_method" "item_post"{
     http_method = "POST"
     authorization = "NONE"
 }
+
+resource "aws_api_gateway_method" "item_delete"{
+    rest_api_id = aws_api_gateway_rest_api.http-crud-tutorial-api.id
+    resource_id = aws_api_gateway_resource.item.id
+    http_method = "DELETE"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "item_put"{
+    rest_api_id = aws_api_gateway_rest_api.http-crud-tutorial-api.id
+    resource_id = aws_api_gateway_resource.item.id
+    http_method = "PUT"
+    authorization = "NONE"
+}
