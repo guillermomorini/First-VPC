68c506c3f72fe493adcc2205be08bc6712c3714e
guillermomorini <guillermo.morini@gmail.com>
2022-11-26 10:35:38 -0500

    updating lambda message

--- terraform/lambda/lambdacode.js
+++ terraform/lambda/lambdacode.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Yo yo from Lambda!'),
     };
     return response;
 };
